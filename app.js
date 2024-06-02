a3287ae49d23b9d8a7145a0d2ae955e1622e43d1
AbigailSotomayor <abigail.sotomayor@hotmail.com>
2024-06-02 17:41:34 +0200

    filering working

--- app.js
+++ app.js
@@ -6,7 +6,69 @@ const cors = require("cors");
 
 app.use(cors());
 app.get("/courses", (req, res) => {
-  res.send(courses);
+  const { query, university, term, ects, degree } = req.query;
+  let filteredCourses = courses;
+
+  // handling query
+  if (query) {
+    filteredCourses = filteredCourses.filter((course) => {
+      return course.title.toLowerCase().includes(query.toLowerCase());
+    });
+  }
+
+  // handling university
+  if (university) {
+    if (Array.isArray(university)) {
+      filteredCourses = filteredCourses.filter((course) =>
+        university.includes(course.university.toLowerCase())
+      );
+    } else {
+      filteredCourses = filteredCourses.filter(
+        (course) => course.university.toLowerCase() === university
+      );
+    }
+  }
+
+  // handling term
+  if (term) {
+    if (Array.isArray(term)) {
+      filteredCourses = filteredCourses.filter((course) =>
+        term.includes(course.term.toLowerCase())
+      );
+    } else {
+      filteredCourses = filteredCourses.filter(
+        (course) => course.term.toLowerCase() === term
+      );
+    }
+  }
+
+  // handling ects
+  if (ects) {
+    if (Array.isArray(ects)) {
+      filteredCourses = filteredCourses.filter((course) =>
+        ects.includes(course.ects.toString())
+      );
+    } else {
+      filteredCourses = filteredCourses.filter(
+        (course) => course.ects === parseInt(ects)
+      );
+    }
+  }
+
+  // handling degree
+  if (degree) {
+    if (Array.isArray(degree)) {
+      filteredCourses = filteredCourses.filter((course) =>
+        degree.includes(course.degree.toLowerCase())
+      );
+    } else {
+      filteredCourses = filteredCourses.filter(
+        (course) => course.degree.toLowerCase() === degree
+      );
+    }
+  }
+
+  res.send(filteredCourses);
 });
 
 app.get("/courses/:university", (req, res) => {
